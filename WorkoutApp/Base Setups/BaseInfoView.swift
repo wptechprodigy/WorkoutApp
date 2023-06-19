e97230ba77856f7e4326d5042bf30f696f8dd738
Waheed Afolabi <waheed.freelancer16@gmail.com>
2023-06-19 17:18:41 +0100

    Complete base setup for info view on `SessionView`

--- WorkoutApp/Base Setups/BaseInfoView.swift
+++ WorkoutApp/Base Setups/BaseInfoView.swift
@@ -26,6 +26,20 @@ class BaseInfoView: BaseView {
         v.layer.borderWidth = 1
         return v
     }()
+    
+    // MARK: - Initializer
+    
+    init(with title: String? = nil,
+         alignment: NSTextAlignment = .left
+    ) {
+        titleLabel.text = title?.uppercased()
+        titleLabel.textAlignment = alignment
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension BaseInfoView {
@@ -36,12 +50,17 @@ extension BaseInfoView {
     
     override func constrainViews() {
         super.constrainViews()
+        
+        /// Handle when the title label is nil
+        let contentViewTopAnchor: NSLayoutAnchor = titleLabel.text == nil ? topAnchor : titleLabel.bottomAnchor
+        let contentViewOffset: CGFloat = titleLabel.text == nil ? 0 : 10
+        
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             
-            contentView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            contentView.topAnchor.constraint(equalTo: contentViewTopAnchor, constant: contentViewOffset),
             contentView.trailingAnchor.constraint(equalTo: trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
             contentView.leadingAnchor.constraint(equalTo: leadingAnchor)
--- WorkoutApp/Views/session/SessionViewController.swift
+++ WorkoutApp/Views/session/SessionViewController.swift
@@ -11,17 +11,26 @@ class SessionViewController: BaseViewController {
     
     // MARK: - Properties
     
-    
+    private let timerView: BaseInfoView = {
+        let v = BaseInfoView(with: "Test", alignment: .center)
+        return v
+    }()
 }
 
 extension SessionViewController {
     
     override func setupViews() {
         super.setupViews()
+        view.setupView(timerView)
     }
     
     override func constrainViews() {
         super.constrainViews()
+        NSLayoutConstraint.activate([
+            timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
     }
     
     override func configureAppearance() {
