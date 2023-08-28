1e88c953023cf8a872bca051d3d9e6d9b0d8102e
Waheed Afolabi <waheed.freelancer16@gmail.com>
2023-08-28 11:51:37 +0100

    Draw the circular progress bar

--- WorkoutApp/Views/session/Views/Timer/ProgressView.swift
+++ WorkoutApp/Views/session/Views/Timer/ProgressView.swift
@@ -10,8 +10,26 @@ import UIKit
 extension TimerView {
     
     final class ProgressView: UIView {
-        func drawProgress(with percent: Int) {
+        func drawProgress(with percent: CGFloat) {
+            let circleFrame = UIScreen.main.bounds.width - (16 + 40) * 2
+            let radius = circleFrame / 2
+            let center = CGPoint(x: radius, y: radius)
+            let startAngle = -CGFloat.pi * 7 / 6
+            let endAngle = CGFloat.pi * 1 / 6
+            let circlePath = UIBezierPath(arcCenter: center,
+                                          radius: radius,
+                                          startAngle: startAngle,
+                                          endAngle: endAngle,
+                                          clockwise: true)
+            let circleLayer = CAShapeLayer()
+            circleLayer.path = circlePath.cgPath
+            circleLayer.strokeColor = R.Colors.active.cgColor
+            circleLayer.lineWidth = 20
+            circleLayer.strokeEnd = percent
+            circleLayer.fillColor = UIColor.clear.cgColor
+            circleLayer.lineCap = .round
             
+            layer.addSublayer(circleLayer)
         }
     }
 }
