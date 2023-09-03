154ecc68b5472765f61f7bb03d1b394b117ceecf
Waheed Afolabi <waheed.freelancer16@gmail.com>
2023-09-03 16:47:31 +0100

    Add logic to start, pause and finish progress

--- WorkoutApp/Views/session/Views/Timer/TimerView.swift
+++ WorkoutApp/Views/session/Views/Timer/TimerView.swift
@@ -7,13 +7,75 @@
 
 import UIKit
 
+enum TimerState {
+    case isRunning
+    case isPaused
+    case isStopped
+}
+
 final class TimerView: WABaseInfoView {
     
-    private let progressView: ProgressView = {
-        let v = ProgressView()
-        v.drawProgress(with: 0.5)
-        return v
-    }()
+    // MARK: - Properties
+    
+    private let progressView = ProgressView()
+    private var timer = Timer()
+    private var timerProgress: CGFloat = 0
+    private var timerDuration = 0.0
+    public var state: TimerState = .isStopped
+    
+    // MARK: -
+    
+    func configure(with duration: Double, progress: Double) {
+        self.timerDuration = duration
+        
+        let tempCurrentValue = progress > duration ? duration : progress
+        let goalDividerValue = duration == 0 ? 1 : duration
+        let percent = tempCurrentValue / goalDividerValue
+        progressView.drawProgress(with: CGFloat(percent))
+    }
+    
+    func startTimer() {
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01,
+                                     repeats: true,
+                                     block: { [weak self] timer in
+            guard let self = self else { return }
+            
+            self.timerProgress += 0.01
+            
+            if self.timerProgress == self.timerDuration {
+                self.timerProgress = self.timerDuration
+                timer.invalidate()
+            }
+            
+            configure(with: self.timerDuration, progress: self.timerProgress)
+        })
+    }
+    
+    func pauseTimer() {
+        timer.invalidate()
+    }
+    
+    func stopTimer() {
+        guard self.timerProgress > 0 else { return }
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01,
+                                     repeats: true,
+                                     block: { [weak self] timer in
+            guard let self = self else { return }
+            
+            self.timerProgress -= 0.1
+            
+            if self.timerProgress <= 0 {
+                self.timerProgress = 0
+                timer.invalidate()
+            }
+            
+            configure(with: self.timerDuration, progress: self.timerProgress)
+        })
+    }
 }
 
 extension TimerView {
